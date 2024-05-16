b98515b16fba7f779c437cb80216231833be53bd
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-05-16 19:50:21 +0000

    required routes built

--- app.rb
+++ app.rb
@@ -2,8 +2,37 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:home)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do 
+
+end
+
+get("/square_root/new") do
+
+end
+
+get("/square_root/results") do
+
+end
+
+get("/random/new") do
+
+end
+
+get("/random/results") do
+
+end
+
+get("payment/new") do
+
+end
+
+get("payment/results") do
+
 end
